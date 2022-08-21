ad17e2cf3b2ae8751a5a318715a596e8538aa7c5
Md Anas Sabah <mdanassabah@gmail.com>
2022-08-21 14:57:35 +0530

    Add: Props

--- src/components/pinned/Pinned.js
+++ src/components/pinned/Pinned.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Pinned.css";
 
-function Pinned() {
+function Pinned({ title, tool }) {
   return (
     <div className="pinned">
       <div className="pinned__title">
@@ -16,7 +16,7 @@ function Pinned() {
           >
             <path d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A5.921 5.921 0 0 1 5 6.708V2.277a2.77 2.77 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354z" />
           </svg>
-          <a href="./">Dice Game</a>
+          <a href="./">{title}</a>
           <p className="public">Public</p>
         </div>
         <div className="pinned__tools">
@@ -24,7 +24,7 @@ function Pinned() {
             className="tool__color"
             style={{ backgroundColor: "rgb(86,61,124)" }}
           ></div>
-          <p>CSS</p>
+          <p>{tool}</p>
         </div>
       </div>
 
--- src/pages/overview/Overview.js
+++ src/pages/overview/Overview.js
@@ -7,12 +7,12 @@ function Overview() {
     <div className="overview">
       <p>Pinned</p>
       <div className="overview__pinned">
-        <Pinned />
-        <Pinned />
-        <Pinned />
-        <Pinned />
-        <Pinned />
-        <Pinned />
+        <Pinned title="dice-game" tool="css" />
+        <Pinned title="guess-my-number" tool="javascript" />
+        <Pinned title=" minimalist-banking" tool="javascript" />
+        <Pinned title=" camera-api " tool="html" />
+        <Pinned title="MAPBOX-API" tool="javascript" />
+        <Pinned title="Movie-Info-Search" tool="javascript" />
       </div>
     </div>
   );
